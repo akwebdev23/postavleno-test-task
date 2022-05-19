621678c903a49202e641dfa57b016be7140ae563
ak <akwebdev23@gmail.com>
2022-05-19 06:15:40 +0300

    storage env index

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+// require_once('./class_auto_load.php');
 require_once('./vendor/autoload.php');
 
 use App\App;
 
 define('STORAGE', 'redis');
+define('STORAGE_HOST', '127.0.0.1');
+define('STORAGE_PORT', 6379);
 define('EXPIRE', 3600);
 
 $app = new App();
 $app->start();
-
 ?>
